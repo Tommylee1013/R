cecc14e08d2e48a170064dd5af25676beaf2fdc0
TommyLee <junghun1013@icloud.com>
2023-07-11 01:59:53 +0900

    Saving

---
+++ Basic Grammer I.R
@@ -0,0 +1,18 @@
+# comment is here
+
+# 산술 연산자
+print(2 + 2) # 덧셈
+print(2 - 1) # 뺄셈
+print(2 * 2) # 곱셈
+print(4 / 2) # 나눗셈
+print(5 %/% 2) # 몫
+print(5 %% 2) # 나머지
+print((3 + 2) ^ 3) # 거듭제곱
+
+# Variables
+x = 1 + 2 # 선언과 초기화가 한번에 이뤄진다
+print(x)
+y <- 10 * 10 # 화살표를 이용해 초기화를 하는 경우가 많다
+print(y)
+1 + 3 -> z # 역방향 초기화도 가능하다
+print(z)
